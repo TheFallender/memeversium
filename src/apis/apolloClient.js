1f02c33281627d81d7d6859415103a12bc2d8734
TheFallender (Agustin) <agustin-ferrari@hotmail.com>
2020-05-10 02:57:09 +0200

    Updated the backend location

--- src/apis/apolloClient.js
+++ src/apis/apolloClient.js
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   	cache: new InMemoryCache(),
   	link: new HttpLink({
-		uri: 'http://thefallenstudio.dnset.com:4000/',
+		uri: 'https://thefallenstudio.dnset.com:4000/',
 	}),
 	onError: ({ networkError, graphQLErrors }) => {
 		console.log('graphQLErrors', graphQLErrors)
